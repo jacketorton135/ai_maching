ecdfac6f9790e6cf08c5032d03e65168d9a9136b
jacketorton135 <ortonhalo@gmail.com>
2024-07-31 00:44:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,28 @@
 import json
-from openai import OpenAI
 from flask import Flask, request, abort
 import os
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 from thingspeak import Thingspeak
+import openai
 
 app = Flask(__name__)
+
+# 確保環境變量正確設置
 line_bot_api_key = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN')
 line_bot_secret_key = os.environ.get('LINE_CHANNEL_SECRET_KEY')
+openai_api_key = os.environ.get('OPENAI_API_KEY')
 
 # Channel Access Token
 line_bot_api = LineBotApi(line_bot_api_key)
 # Channel Secret
 handler = WebhookHandler(line_bot_secret_key)
 
-# Open AI key
-openai_api_key = os.environ.get('OPEN_AI_KEY')
-
-# U5583a266be8eb6b47ad9fa7d96846c80
 # Auth User list
-auth_user_list = os.environ.get('AUTH_USER_LIST')  # string
-auth_user_list = auth_user_list.split(',')  #list
+auth_user_list = os.environ.get('AUTH_USER_LIST', '').split(',')
+auth_user_ai_list = os.environ.get('AUTH_USER_AI_LIST', '').split(',')
 print('auth_user_list', auth_user_list)
-
-# U5583a266be8eb6b47ad9fa7d96846c80
-# Auth AI User list
-auth_user_ai_list = os.environ.get('AUTH_AI_USER_LIST')  # string
-auth_user_ai_list = auth_user_ai_list.split(',')  #list
 print('auth_user_ai_list', auth_user_ai_list)
 
 # 監聽所有來自 /callback 的 Post Request
@@ -46,63 +40,53 @@ def callback():
         abort(400)
     return 'OK'
 
-# input:  "圖表:2466473,field1,GROLYCVTU08JWN8Q"
-# 處理訊息
+# 處理消息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_request_user_id = event.source.user_id
     print('get_request_user_id', get_request_user_id)
     input_msg = event.message.text
     check = input_msg[:3].lower()
-    user_msg = input_msg[3:]  # 2466473,field1,GROLYCVTU08JWN8Q
+    user_msg = input_msg[3:]  # 2374700,2KNDBSF9FN4M5EY1
     print('check', check)
     print('user_msg', user_msg)
     if get_request_user_id in auth_user_list:
-
-        if check in "圖表:":
+        if check == "圖表:":
             channel_id = user_msg.split(',')[0]
-            field = user_msg.split(',')[1]
-            key = user_msg.split(',')[2]
-            print("User channel_id: ", channel_id, "Field: ", field, "Read_key: ", key)
+            key = user_msg.split(',')[1]
+            print("User channel_id: ", channel_id, "Read_key: ", key)
             ts = Thingspeak()
-            tw_time_list, bpm_list = ts.get_data_from_thingspeak(
-                channel_id, field, key)
+            tw_time_list, bpm_list, temperature_list, humidity_list, body_temperature_list, ECG_list = ts.get_data_from_thingspeak(channel_id, key)
             if tw_time_list == 'Not Found' or bpm_list == 'Not Found':
                 message = TextSendMessage(text="User not found")
                 line_bot_api.reply_message(event.reply_token, message)
             else:
-                ts.gen_chart(tw_time_list, bpm_list)
+                ts.gen_chart(tw_time_list, bpm_list, temperature_list, humidity_list, body_temperature_list, ECG_list)
                 ts.update_photo_size()
-                chart_link, pre_chart_link = ts.upload_to_imgur()
-                print("圖片網址", chart_link)
-                print("縮圖網址", pre_chart_link)
-                image_message = ImageSendMessage(
-                    original_content_url=chart_link,
-                    preview_image_url=pre_chart_link)
-                line_bot_api.reply_message(event.reply_token, image_message)
+                chart_links, pre_chart_links = ts.upload_to_imgur()
+                print("圖片網址", chart_links)
+                print("縮圖網址", pre_chart_links)
+
+                messages = [ImageSendMessage(original_content_url=chart_link, preview_image_url=pre_chart_link)
+                            for chart_link, pre_chart_link in zip(chart_links, pre_chart_links)]
+                line_bot_api.reply_message(event.reply_token, messages)
         elif check == 'ai:' and get_request_user_id in auth_user_ai_list:
             try:
-                client = OpenAI(api_key=openai_api_key)
-
-                response = client.chat.completions.create(
-                    model="gpt-3.5-turbo-0125",
-                    messages=[
-                        {
-                            "role": "system",
-                            "content": "如果回答問題盡可能用簡潔的話回復"
-                        },
-                        {
-                            "role": "user",
-                            "content": user_msg,
-                        },
-                    ],
+                openai.api_key = openai_api_key
+                response = openai.Completion.create(
+                    model="gpt-3.5-turbo",
+                    prompt=user_msg,
+                    temperature=0.5,
+                    max_tokens=500
                 )
-                reply_msg = response.choices[0].message.content
+                reply_msg = response.choices[0].text.strip()
                 print('reply_msg', reply_msg)
                 message = TextSendMessage(text=reply_msg)
                 line_bot_api.reply_message(event.reply_token, message)
             except Exception as e:
                 print(e)
+                message = TextSendMessage(text='Error with OpenAI API')
+                line_bot_api.reply_message(event.reply_token, message)
         else:  # 學使用者說話
             message = TextSendMessage(text=event.message.text)
             line_bot_api.reply_message(event.reply_token, message)
@@ -110,13 +94,9 @@ def handle_message(event):
         message = TextSendMessage(text='使用者沒有權限')
         line_bot_api.reply_message(event.reply_token, message)
 
-
 if __name__ == "__main__":
-    # port = int(os.environ.get('PORT', 5001))
-    # local
-    # app.run(host='0.0.0.0', port=5001)
-    # Render
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
